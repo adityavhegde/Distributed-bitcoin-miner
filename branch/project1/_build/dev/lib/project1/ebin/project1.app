a763fcd2dd38d9fc041f153aa73c02e6616d4c23
Kumar Rohit Malhotra <krohitm@gmail.com>
2017-09-10 01:26:58 -0400

    rearranged

---
+++ branch/project1/_build/dev/lib/project1/ebin/project1.app
@@ -0,0 +1,6 @@
+{application,project1,
+             [{description,"project1"},
+              {modules,['Elixir.GenHash','Elixir.Project1']},
+              {registered,[]},
+              {vsn,"0.1.0"},
+              {applications,[kernel,stdlib,elixir,logger]}]}.
--- lib/project1.ex
+++ lib/project1.ex
@@ -1,18 +1,31 @@
-defmodule Project1 do
-  @moduledoc """
-  Documentation for Project1.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Project1.hello
-      :world
-
-  """
-  def hello do
-    :world
+defmodule GenHash do
+  def stringToHash(string) do
+    #generate SHA-256 from string and convert to lowercase
+    :crypto.hash(:sha256, string) |> Base.encode16 |> String.downcase
   end
 end
+
+defmodule RC do
+  def perm_rep(list), do: perm_rep(list, length(list))
+  def perm_rep([], _), do: [[]]
+  def perm_rep(_,  0), do: [[]]
+  def perm_rep(list, i) do
+    for x <- list, y <- perm_rep(list, i-1), do: [x|y]
+  end
+end
+ 
+list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+#list = ['a','b','c']
+#Enum.each(3..3, fn n ->
+#  IO.inspect RC.perm_rep(list,n)
+#end)
+Enum.each(1..26, fn n ->
+  Enum.each(RC.perm_rep(list,n), fn header ->
+      cond do
+        Enum.join(["ahegde",header], "") |> GenHash.stringToHash |> String.slice(0..2) == "000" ->
+          IO.puts Enum.join(["ahegde",header], "") |> GenHash.stringToHash
+        true ->
+          true#IO.puts 1#Enum.join(["ahegde",header], "") |> GenHash.stringToHash
+      end
+    end)
+  end)
