97a3a5f60ce75e5e80dcdf2f39a70e32e5824145
Kumar Rohit Malhotra <krohitm@gmail.com>
2017-09-12 12:08:12 -0400

    spawning processes

--- lib/project1.ex
+++ lib/project1.ex
@@ -1,31 +1,68 @@
-defmodule GenHash do
-  def stringToHash(string) do
-    #generate SHA-256 from string and convert to lowercase
-    :crypto.hash(:sha256, string) |> Base.encode16 |> String.downcase
-  end
+defmodule Chain do
+    def counter(next_pid) do
+        numLeadingZeros =4
+        list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+        'o','p','q','r','s','t','u','v','w','x','y','z','`','1','2','3',
+        '4','5','6','7','8','9','0', '-', '=', '[', ']', '\\', ';', '\'',
+        ',', '.', '/', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', 
+        ')', '_', '+', '{', '}', '|', ':', '\"', '<', '>', '?']
+        receive do
+            n ->
+                send next_pid, n + 1
+                GenHash.perm_rep(list,[[]],n, numLeadingZeros-1)
+                #send next_pid, n + 1
+        end
+    end
+    
+    def create_processes(n) do
+        last = Enum.reduce 1..n, self(),
+        fn (_,send_to) ->
+            spawn(Chain, :counter, [send_to])
+        end
+    
+        send last, 1 # start the count by sending a 1 to the last process
+
+        receive do # and wait for the result to come back to us
+            final_answer when is_integer(final_answer) ->
+                "Result is #{inspect(final_answer)}"
+        end
+    end
+
+    def run(n) do
+        IO.puts inspect :timer.tc(Chain, :create_processes, [n])
+        #Chain.create_processes(n)
+    end
 end
 
-defmodule RC do
-  def perm_rep(list), do: perm_rep(list, length(list))
-  def perm_rep([], _), do: [[]]
-  def perm_rep(_,  0), do: [[]]
-  def perm_rep(list, i) do
-    for x <- list, y <- perm_rep(list, i-1), do: [x|y]
-  end
+defmodule GenHash do
+    #generate SHA-256 from string and convert to lowercase
+    def stringToHash(string) do
+        :crypto.hash(:sha256, string) |> Base.encode16 |> String.downcase
+    end
+
+    #generates permutations and checks for hash with leading zeroes
+    def perm_rep(list1, list2, i, leadingZeros) do
+        #comprehend through the two lists
+        for x <- list1, y <- list2 do
+            cond do
+                #go till i and check only for that sized suffix
+                i == 1 ->
+                #concatenate prefix with generated permutation
+                header = ["adityavhegde;"|[x|y]]
+                hash = header |> stringToHash
+                cond do
+                    #output the string and its hash if found with required number of leading zeros
+                    hash |> String.slice(0..leadingZeros) == "0000" ->
+                        IO.puts [header, "    ", hash]
+                    true ->
+                        true
+                end
+                i != 1 ->
+                    #recursively call the function with primary list and the concatenation of current x and y as inputs
+                    perm_rep(list1,[[x|y]], i-1, leadingZeros)
+            end
+        end
+    end
 end
- 
-list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-#list = ['a','b','c']
-#Enum.each(3..3, fn n ->
-#  IO.inspect RC.perm_rep(list,n)
-#end)
-Enum.each(1..26, fn n ->
-  Enum.each(RC.perm_rep(list,n), fn header ->
-      cond do
-        Enum.join(["ahegde",header], "") |> GenHash.stringToHash |> String.slice(0..2) == "000" ->
-          IO.puts Enum.join(["ahegde",header], "") |> GenHash.stringToHash
-        true ->
-          true#IO.puts 1#Enum.join(["ahegde",header], "") |> GenHash.stringToHash
-      end
-    end)
-  end)
+
+Chain.run(200_000)
